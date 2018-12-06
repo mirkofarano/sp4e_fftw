e51d8272bf28c48995df75fdbf4378481a76f669
Mirko Farano <m.farano@libero.it>
2018-12-06 18:34:19 +0100

    TODO ex 4

--- src/test_fft.cc
+++ src/test_fft.cc
@@ -25,9 +25,9 @@ TEST(FFT, transform) {
       std::cout << i << "," << j << " = " << val << std::endl;
 
     if (i == 1 && j == 0)
-      ASSERT_NEAR(std::abs(val), N * N / 2, 1e-10);
+      ASSERT_NEAR(std::abs(val), 1.0/2.0, 1e-10);
     else if (i == N - 1 && j == 0)
-      ASSERT_NEAR(std::abs(val), N * N / 2, 1e-10);
+      ASSERT_NEAR(std::abs(val), 1.0/2.0, 1e-10);
     else
       ASSERT_NEAR(std::abs(val), 0, 1e-10);
   }
@@ -35,5 +35,32 @@ TEST(FFT, transform) {
 /*****************************************************************/
 
 TEST(FFT, inverse_transform) {
+  UInt N = 512;
+  Matrix<complex> m(N);
+
+  Real k = 2 * M_PI / N;
+  for (auto&& entry : index(m)) {
+    int i = std::get<0>(entry);
+    int j = std::get<1>(entry);
+    auto& val = std::get<2>(entry);
+    val = cos(k * i);
+  }
+
+  Matrix<complex> res = FFT::itransform(m);
+
+  for (auto&& entry : index(res)) {
+    int i = std::get<0>(entry);
+    int j = std::get<1>(entry);
+    auto& val = std::get<2>(entry);
+    if (std::abs(val) > 1e-10)
+      std::cout << i << "," << j << " = " << val << std::endl;
+
+    if (i == 1 && j == 0)
+      ASSERT_NEAR(std::abs(val), N * N / 2, 1e-10);
+    else if (i == N - 1 && j == 0)
+      ASSERT_NEAR(std::abs(val), N * N / 2, 1e-10);
+    else
+      ASSERT_NEAR(std::abs(val), 0, 1e-10);
+  }
 }
 /*****************************************************************/
